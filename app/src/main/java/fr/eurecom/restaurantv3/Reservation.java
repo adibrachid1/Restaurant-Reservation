6577e7aecea0fd651fa828117c7c57c36a7ca069
root <root@ADIBPC.localdomain>
2021-01-29 00:44:00 +0100

    Added reservation plan, on click on image coordinates

--- app/src/main/java/fr/eurecom/restaurantv3/Reservation.java
+++ app/src/main/java/fr/eurecom/restaurantv3/Reservation.java
@@ -1,16 +1,46 @@
 package fr.eurecom.restaurantv3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.graphics.Color;
+import android.graphics.Path;
 import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TimePicker;
 
-public class Reservation extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.squareup.picasso.Picasso;
+
+import java.util.Calendar;
+
+public class Reservation extends AppCompatActivity implements
+        View.OnClickListener {
+    int[] viewCoords = new int[2];
 
     String resto_id;
     String resto_name;
+    Button btnDatePicker, btnTimePicker;
+    EditText txtDate, txtTime;
+    private int mYear, mMonth, mDay, mHour, mMinute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +51,69 @@ public class Reservation extends AppCompatActivity {
         getSupportActionBar().setTitle("Reservation - "+resto_name);
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#B9CF5A0C")));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //data time
+        btnDatePicker=(Button)findViewById(R.id.btn_date);
+        btnTimePicker=(Button)findViewById(R.id.btn_time);
+        txtDate=(EditText)findViewById(R.id.in_date);
+        txtTime=(EditText)findViewById(R.id.in_time);
+        btnDatePicker.setOnClickListener(this);
+        btnTimePicker.setOnClickListener(this);
     }
+
+    public void reserve(View view){
+
+    }
+
+    public void load_plan(){
+        Button b = (Button) findViewById(R.id.button_reserve);
+        b.setVisibility(View.VISIBLE);
+        ImageView a = (ImageView)findViewById(R.id.imageView_plan);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("country")
+                .document("France")
+                .collection("postal")
+                .document("06000")
+                .collection("restaurants")
+                .document(resto_id)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            ImageView a = (ImageView)findViewById(R.id.imageView_plan);
+                            Picasso.with(getApplicationContext()).load(task.getResult().getData().get("plan").toString()).into(a);
+
+                        } else {
+                            Log.w("TAG", "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+        a.getLocationOnScreen(viewCoords);
+        a.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                //if(event.getAction() == MotionEvent.ACTION_UP){
+
+                    int touchX = (int) event.getX();
+                    int touchY = (int) event.getY();
+
+                    int imageX = touchX - viewCoords[0]; // viewCoords[0] is the X coordinate
+                    int imageY = touchY - viewCoords[1];
+                    Log.d("ADIBBBBBBB","Position: x="+imageX+"|Y="+imageY);
+                    // Do what you want
+                    return true;
+                //}
+                //return false;
+            }
+        });
+
+    }
+
+    public void proceed(View view){
+        load_plan();
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -30,5 +122,51 @@ public class Reservation extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public void onClick(View v) {
+
+        if (v == btnDatePicker) {
+
+            // Get Current Date
+            final Calendar c = Calendar.getInstance();
+            mYear = c.get(Calendar.YEAR);
+            mMonth = c.get(Calendar.MONTH);
+            mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                    new DatePickerDialog.OnDateSetListener() {
+
+                        @Override
+                        public void onDateSet(DatePicker view, int year,
+                                              int monthOfYear, int dayOfMonth) {
+
+                            txtDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                        }
+                    }, mYear, mMonth, mDay);
+            datePickerDialog.show();
+        }
+        if (v == btnTimePicker) {
+
+            // Get Current Time
+            final Calendar c = Calendar.getInstance();
+            mHour = c.get(Calendar.HOUR_OF_DAY);
+            mMinute = c.get(Calendar.MINUTE);
+
+            // Launch Time Picker Dialog
+            TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                    new TimePickerDialog.OnTimeSetListener() {
+
+                        @Override
+                        public void onTimeSet(TimePicker view, int hourOfDay,
+                                              int minute) {
+
+                            txtTime.setText(hourOfDay + ":" + minute);
+                        }
+                    }, mHour, mMinute, false);
+            timePickerDialog.show();
+        }
     }
 }
